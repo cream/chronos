ccdb58634a6a063303fba2554153046150d8cfee
Kristoffer Kleine <kris.kleine@yahoo.de>
2011-07-17 21:33:19 +0200

    Removed unused attribute

--- src/chronos/event.py
+++ src/chronos/event.py
@@ -16,8 +16,6 @@ class Event(object):
         self.color = color
         self.active = active
 
-        self.positions = [0] * 7
-
         if isinstance(start, (float, int)):
             self.start = datetime.fromtimestamp(start)
         if isinstance(end, (float, int)):
